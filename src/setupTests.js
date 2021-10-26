4488057189e0cd07ddae96b7d9083e8fc4392ac5
Pablo Tamarit <pablo@ptamarit.com>
2021-10-26 13:47:46 +0200

    Setting defaultHidden and overriding slow getComputedStyle

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,29 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import { configure } from '@testing-library/react';
+
+// Applying changes to speed up tests locally.
+// These changes are not applied on the Continuous Integration environment.
+if (!process.env.CI) {
+  // Skipping the checks on elements visibility for ByRole queries, since this is quite slow.
+  configure({ defaultHidden: true });
+
+  // Setting defaultHidden is not enough to improve performance when using ByRole with big DOM structures,
+  // since a lot of time is also spent checking visibilities for determining accessible names.
+  // Therefore, we override getComputedStyle to make elements always visible.
+  // See: https://github.com/eps1lon/dom-accessibility-api/discussions/605
+  Object.defineProperty(window, 'getComputedStyle', {
+    value: () => ({
+      getPropertyValue: (prop) => {
+        if (prop === 'display') {
+          return 'block';
+        } else if (prop === 'visibility') {
+          return 'visible';
+        } else {
+          return '';
+        }
+      },
+    }),
+  });
+}
